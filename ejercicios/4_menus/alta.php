8fe632b5c9609739913c9eb664a0c3dcd29109e4
angelporlan <angeelporlan4@gmail.com>
2024-01-10 19:36:16 +0100

    terminado lo de añadir usuarios

--- ejercicios/4_menus/alta.php
+++ ejercicios/4_menus/alta.php
@@ -1,3 +1,28 @@
+<?php
+session_start();
+
+if (isset($_SESSION["errorEmail"])) {
+    $errorEmail = $_SESSION["errorEmail"];
+}
+
+if (isset($_SESSION["errorPassword"])) {
+    $errorPassword = $_SESSION["errorPassword"];
+}
+
+if (isset($_SESSION["errorFichero"])) {
+    $errorFichero = $_SESSION["errorFichero"];
+}
+
+if (isset($_SESSION["dataOK"])) {
+    $dataOK = $_SESSION["dataOK"];
+}
+
+// print "<pre>";
+// print "Matriz \$_SESSION" . "<br>";
+// print_r($_SESSION);
+// print "</pre>\n";
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,7 +37,7 @@
 <?php include "menu.php"?>
 
     <main>
-        <form action="">
+        <form action="controlador/procesar_alta.php" method="post" enctype="multipart/form-data">
             <header>Alta</header>
             <div class="content">
             <div class="left">
@@ -24,13 +49,30 @@
                 <p>Foto de Perfil (max 1MB):</p>
             </div>
             <div class="right">
-                <input type="text" name="firtname" id="">
-                <input type="text" name="lastname" id="">
-                <input type="text" name="email" id="">
-                <input type="password" name="pass1" id="">
-                <input type="password" name="pass2" id="">
-                <input type="submit" value="">
+                <input type="text" name="firstname" value="<?php echo !empty($_SESSION['usuario']) ? $_SESSION["usuario"]["name"] : ''; ?>">
+                <input type="text" name="lastname"  value="<?php echo !empty($_SESSION['usuario']) ? $_SESSION["usuario"]["lastname"] : ''; ?>">
+                <input type="text" name="email" value="<?php echo !empty($_SESSION['usuario']) ? $_SESSION["usuario"]["email"] : ''; ?>">
+                <input type="password" name="pass1" value="<?php echo !empty($_SESSION['usuario']) ? $_SESSION["usuario"]["pass1"] : ''; ?>">
+                <input type="password" name="pass2" value="<?php echo !empty($_SESSION['usuario']) ? $_SESSION["usuario"]["pass2"] : ''; ?>">
+                <input type="file" name="photo">
             </div>
+            <?php
+        if (isset($errorEmail)) {
+            print "<p class='error'>$errorEmail</p>";
+        }
+        if (isset($errorPassword)) {
+            print "<p class='error'>$errorPassword</p>";
+        }
+        if (isset($errorFichero)) {
+            print "<p class='error'>$errorFichero</p>";
+        }
+
+        if (isset($dataOK) && $dataOK == true) {
+            print "<p class='dataok'>Alta de usuario correcta</p>";
+            unset($_SESSION["dataOK"]); 
+        }
+
+        ?>
             </div>
             <button>Alta de nuevo usuario</button>
             <p>* Campos obligatorios</p>
---
+++ ejercicios/4_menus/controlador/procesar_alta.php
@@ -0,0 +1,104 @@
+<?php
+    session_start();
+    require_once('../modelo/modelo.php');
+    require_once('../includes/funciones.php');
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $name = recoge("firstname");
+        $lastname = recoge("lastname");
+        $email = recoge("email");
+        $pass1 = recoge("pass1");
+        $pass2 = recoge("pass2");
+
+        $_SESSION["user"]["name"] = $name;
+        $_SESSION["user"]["lastname"] = $lastname;
+        $_SESSION["user"]["email"] = $email;
+        $_SESSION["user"]["pass1"] = $pass1;
+        $_SESSION["user"]["pass2"] = $pass2;
+
+        if ($_FILES["photo"]["error"] == UPLOAD_ERR_OK) {
+            $photo = $_FILES["photo"]["name"];
+            $tamBytes = $_FILES["photo"]["size"];
+        } else {
+            print("no hay fichero");
+        }
+        
+        $_SESSION["dataOK"] = true;
+        //email
+        if (!str_contains($email, '@') || !str_contains($email, '.')) {
+            $_SESSION["errorEmail"] = "Email inválido. Debe contener @ y .";
+            $_SESSION["dataOK"] = false;
+        } else if (existeUsuario($email)) {
+            $_SESSION["errorEmail"] = "El email ya existe. Use otro distinto";
+            $_SESSION["dataOK"] = false;
+        } else {
+            unset($_SESSION["errorEmail"]);
+        }
+        //password
+        if (empty($pass1) || empty($pass2)) {
+            $_SESSION["errorPassword"] = "Debe ingresar ambas contraseñas";
+            $_SESSION["dataOK"] = false;
+        } elseif ($pass1 !== $pass2) {    
+            $_SESSION["errorPassword"] = "El password no coincide";
+            $_SESSION["dataOK"] = false;
+        } else {
+            unset($_SESSION["errorPassword"]);
+        }
+
+        //Imagen
+        if ($_FILES["photo"]["error"] == UPLOAD_ERR_OK) {
+            if ($_FILES["photo"]["size"] < 1000000) {
+                $ruta_subida = "../bbdd/";
+                $res = move_uploaded_file($_FILES["photo"]["tmp_name"], $ruta_subida . $_FILES["photo"]["name"]);
+
+                if ($res) {
+                    unset($_SESSION["errorFichero"]);
+                } else {
+                    $_SESSION["errorFichero"] = "Error al guardar fichero";
+                }
+            } else {
+                $_SESSION["errorFichero"] = "Tamaño imagen demasiado grande";
+                $_SESSION["dataOK"] = false;
+            }
+        }
+
+        if ($_SESSION["dataOK"] == true) {
+
+            print("<h2>DATOS OK</h2>");
+    
+            //Recupero la lista de usuarios
+            $lista_usuarios = [];
+            $file = '../bbdd/data.json';    //la carpeta bbdd debe existir
+    
+            $jsonData = file_get_contents("./{$file}", FILE_USE_INCLUDE_PATH);
+            $lista_usuarios = json_decode($jsonData);
+    
+    
+            //Me creo el objeto usuario
+            $usuario = new Usuario;
+            $usuario->nombre = $name;
+            $usuario->apellidos = $lastname;
+            $usuario->email = $email;
+            $usuario->password = $pass1;
+            if ($_FILES["photo"]["error"] == UPLOAD_ERR_OK) {
+                $usuario->imagen = $photo;
+            }
+    
+            array_push($lista_usuarios, $usuario);
+            $json_usuarios = json_encode($lista_usuarios, JSON_PRETTY_PRINT);
+            // print "<pre>";
+            // print_r($json_usuarios);
+            // print "</pre>";
+            file_put_contents("../bbdd/data.json", $json_usuarios);
+    
+            unset($_SESSION["usuario"]);
+    
+            //session_destroy();
+        }
+    }
+    
+    
+    header("Location: ../alta.php");
+    
+    
+?>
